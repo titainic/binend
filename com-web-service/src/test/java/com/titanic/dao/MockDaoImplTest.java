46233a1e9330f0875ce133069c8998f340602dd5
titainic <binend@live.cn>
2015-12-11 10:23:48 +0800

    新增mock测试

---
+++ com-web-service/src/test/java/com/titanic/dao/MockDaoImplTest.java
@@ -0,0 +1,46 @@
+package com.titanic.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.stubVoid;
+import static org.mockito.Mockito.when;
+
+/**
+ *mock 测试
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class MockDaoImplTest
+{
+    @Mock
+    MockDaoImpl mockDaoImpl;
+
+
+    @Test
+    public void init()
+    {
+        when(mockDaoImpl.getData()).thenReturn("ssss");
+    }
+
+
+    /**
+     * 无参，void返回类型
+     * @throws Exception
+     */
+    @Test
+    public void testGetData() throws Exception
+    {
+        mockDaoImpl.getData();
+    }
+
+    @Test
+    public void testInit() throws Exception
+    {
+        stubVoid(mockDaoImpl).toReturn().on().init();
+        mockDaoImpl.init();
+    }
+}
