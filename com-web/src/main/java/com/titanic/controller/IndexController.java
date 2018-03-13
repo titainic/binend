0bfa934997c9313616e13d99b0fa538ca1460ba2
杨彬 <binend@live.cn>
2018-03-13 16:23:02 +0800

    web提交任务到spark集群

--- com-web/src/main/java/com/titanic/controller/IndexController.java
+++ com-web/src/main/java/com/titanic/controller/IndexController.java
@@ -1,18 +1,24 @@
 package com.titanic.controller;
 
+import com.titanic.remote.SparkRemoteSubmitJob;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by jiawa on 15/11/4.
  */
 @Controller
+@RequestMapping("spark")
 public class IndexController
 {
 
-    @RequestMapping("/index.htm")
+    @ResponseBody
+    @RequestMapping(value = "sparkSubmit", method = { RequestMethod.GET, RequestMethod.POST })
     public String index()
     {
+        SparkRemoteSubmitJob.submitJob();
         return "index";
     }
 }
